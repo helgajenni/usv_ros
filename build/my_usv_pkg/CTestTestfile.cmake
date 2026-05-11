68c42115532963a8b962509bb0b099962ae2b995
helgajenni <helgapanjaitan05@gmail.com>
2026-05-11 22:31:43 +0700

    Fix: remove build and devel folders, add source code

--- build/my_usv_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/helga/catkin_ws/src/my_usv_pkg
-# Build directory: /home/helga/catkin_ws/build/my_usv_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
